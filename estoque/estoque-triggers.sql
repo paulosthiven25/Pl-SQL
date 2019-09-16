54b81ed1e9b677683f2c4fb0f2bca18552d8c982
Logon Aluno <logonaluno@local.com>
2019-09-16 12:01:07 -0300

    controle de estoque funcionando

--- estoque/estoque-triggers.sql
+++ estoque/estoque-triggers.sql
@@ -5,21 +5,21 @@ FOR EACH ROW
 begin
 
 IF inserting THEN 
-atualizar_estoque(:new.CD_PRODUTO,:new.QT_ESTOQUE);
+atualizar_estoque(:new.CD_PRODUTO,:new.QT_PRODUTO);
 end if;
  
  if UPDATING then
-atualizar_estoque(:new.CD_PRODUTO,:new.QT_ESTOQUE - :old.QT_ESTOQUE);
+atualizar_estoque(:new.CD_PRODUTO,:new.QT_PRODUTO - :old.QT_PRODUTO);
 end if;
 
 if DELETING then
-atualizar_estoque(:old.CD_PRODUTO,:old.QT_ESTOQUE * -1);
+atualizar_estoque(:old.CD_PRODUTO,:old.QT_PRODUTO * -1);
 end if;
 
 end;
 /
 
-
+desc t_est_entrada
 Create or replace trigger trg_saida_produto
 after insert or UPDATE or delete on t_est_saida
 REFERENCING OLD AS old NEW AS new
